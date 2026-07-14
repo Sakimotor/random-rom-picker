74d911143292ebb557dd9c9501010cc6c29ca868
Sakimotor <mambila2001@gmail.com>
2026-07-14 23:54:40 +0200

    slight update

--- rom_picker.py
+++ rom_picker.py
@@ -12,44 +12,6 @@ from lib.doujinstyle_to_json import update_doujinstyle_json
 from tqdm import tqdm
 
 #from https://gist.github.com/phineas-pta/d73f9a035b05f8e923af8c01df057175
-def download(url:str, filename:str="", overwrite:bool=False, chunk_size:int=1) -> None:
-    """
-    try auto detect file name if left empty
-    option to overwrite if file already existed
-    chunk_size in MBytes
-    """
-    HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-               "Referer": "https://minerva.erista.me/"}  # some sites block if u dont have at least user agent
-    with requests.get(url, headers=HEADERS, stream=True) as resp:
-
-        # get file name
-        if filename == "":
-            MISSING_FILENAME = "missing_name"
-            if content_disposition := resp.headers.get("Content-Disposition"):
-                param, options = werkzeug.http.parse_options_header(content_disposition)
-                if param == "attachment":
-                    filename = options.get("filename", MISSING_FILENAME)
-                else:
-                    filename = MISSING_FILENAME
-            else:
-                filename = os.path.basename(url)
-                fileext = os.path.splitext(filename)[-1]
-                if fileext == "":
-                    filename = MISSING_FILENAME
-
-        # download file
-        print(url)
-        if overwrite or not os.path.exists(filename):
-            TOTAL_SIZE = int(resp.headers.get("Content-Length", 0))
-            CHUNK_SIZE = chunk_size * 10**6
-            with (
-                open(filename, mode="wb") as file,
-                tqdm(total=TOTAL_SIZE, desc=f"Downloading {filename}", unit="B", unit_scale=True) as bar
-            ):
-                for data in resp.iter_content(chunk_size=CHUNK_SIZE):
-                    size = file.write(data)
-                    bar.update(size)
-
 console_list = list(Console)
 
 
@@ -105,7 +67,6 @@ def pick_random_game(console:Console=None):
 
                 
             
-            print(f"current console: {console_cur}")
             if console_cur == Console.FB:
                 with open('res/fbneo_roms.json', 'r', encoding='utf-8') as fbneo_file:
                     romlist_fbneo = json.load(fbneo_file)
@@ -122,32 +83,18 @@ def pick_random_game(console:Console=None):
             
             result['console'] = console_cur
             return result
-            
-
-def download_fbrom_and_reqs(res, romlist_fbneo):
-    reqs_cur = res['reqs']
-    if reqs_cur is not None:
-        for req_title in reqs_cur:
-            req_cur = romlist_fbneo[req_title]
-            res_next = dict(title=req_title, reqs= (req_cur['require'] if 'require' in req_cur  else None), link=req_cur['download'])
-            download_fbrom_and_reqs(res_next, romlist_fbneo)
-    download(res['link'], 'roms/' + Console.FB + '/' + res['title'] + '.zip')       
+                  
             
                 
 
 def main():
     parser = argparse.ArgumentParser(
                     prog='ROM/Game Picker',
-                    description='Picks a random game and downloads it by default')
+                    description='Picks a random game')
     
-    parser.add_argument('-nodl', '--no-download', nargs='*')
     parser.add_argument('-noconf', '--no-config', nargs='*')
 
     args = parser.parse_args()
-
-
-    print(args)
-
     res = None
 
 
@@ -162,11 +109,16 @@ def main():
         configure_program()
     
     if args.no_config is None:
-        if "y" == input("Do you want to update the roms list ? (Y/N)").lower():
-            update_roms_json() #commented out until I find a minerva replacement, EDIT: minerva is up and running
-            update_myabandonware_json() #takes too loooong
-            update_abfrance_json()
-            update_doujinstyle_json()
+        sources = [
+            ("Minerva", update_roms_json),
+            ("MyAbandonware", update_myabandonware_json),
+            ("AB-France", update_abfrance_json),
+            ("Doujinstyle", update_doujinstyle_json),
+        ]
+
+        for name, func in sources:
+            if input(f"Update {name} list? (Y/N): ").strip().lower() == "y":
+                func()
 
     
         if "y" in (input("Pick one specific console ? (Y/N)").lower()):
@@ -187,18 +139,7 @@ def main():
     else:
         res = pick_random_game()
     print(f"Today's ROM will be {res['title']} on the {res['console']}!")
-    
-    if args.no_download is None:
-        print(res)
-        if not os.path.exists('roms/' + res['console']):
-            os.makedirs('roms/' + res['console'])
-        if res['console'] == Console.FB:
-            with open('res/fbneo_roms.json', 'r', encoding='utf-8') as fbneo_file:
-                romlist_fbneo = json.load(fbneo_file)
-                download_fbrom_and_reqs(res, romlist_fbneo)
-                    
-        else:
-            download(res['link'],'roms/' + res['console'] + '/' + res['title'] + ('.chd' if "CHD" in res['console'] else '.zip'))
+    print(res['link'])
     
 
 
